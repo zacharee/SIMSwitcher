7d686a7f950502a67e2c2674024e859d99891d82
Zachary Wander <zachary.wander@gmail.com>
2024-05-02 19:09:30 -0400

    Version 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# 1.0.1
+- Fix a crash when grabbing the QS tile icon.
+
 # 1.0.0
 - Initial release.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.zwander.simswitcher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         vectorDrawables {
             useSupportLibrary = true
